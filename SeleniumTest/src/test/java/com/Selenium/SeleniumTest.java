21990a6f8236acb5fbbcca575001dc703d26dbd2
rajoy <rajoy@DESKTOP-18KB1V5.lan>
2021-12-28 20:53:36 -0500

    Adding Selenium Project

---
+++ SeleniumTest/src/test/java/com/Selenium/SeleniumTest.java
@@ -0,0 +1,46 @@
+package com.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+		private WebDriver driver;
+	    private final String url = "https://www.selenium.dev/";;
+
+	    @Test
+	    public void canLaunchWebDriver() {
+	        String currenturl = driver.getCurrentUrl();
+	        Assert.assertEquals(url, currenturl);
+	    }
+
+	    @Test
+	    public void canClickButton() {
+	        WebElement element = driver.findElement(By.cssSelector("a[href='/documentation/webdriver/']"));
+	        element.click();
+	        driver.quit();
+	    }
+
+	    @BeforeMethod
+	    public void setup() {
+	        //String driverpath = "C:\\Users\\Regime Cary\\eclipse-quintrix-training\\driver\\chromedriver.exe";
+	    	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+	        this.driver = new ChromeDriver();
+	        this.driver.manage().window().maximize();
+	        this.driver.navigate().to(url);
+	    }
+	    
+	    @AfterMethod
+	    public void tearDown() {
+	        this.driver.quit();
+	    }
+	
+	}
+
+
