0a34070761e5c17aa5035fa96bb5bebe6263c77a
rajoy <rajoy@DESKTOP-18KB1V5.lan>
2022-01-06 20:20:17 -0500

    adding nested iframes project

--- SeleniumTest/src/test/java/NestedIframesTests/NestedIframestest.java
+++ SeleniumTest/src/test/java/NestedIframesTests/NestedIframestest.java
@@ -6,15 +6,23 @@ import NestedIframesPages.NestedIframes;
 import framework.TestBase;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class NestedIframestest extends TestBase {
   @Test
   public void canTestNestedIframes() {
 	  
+	  String expectedText="Child Iframe";
+	  
 	  NestedIframes nf=new NestedIframes(this.getDriver()).navigate();
 	  
-	  System.out.println(nf.getText());
+	  //System.out.println(nf.getText());
+	  String actualText=nf.getText();
+	  
+	  Assert.assertEquals(actualText, expectedText);
+	  
+	  
   }
   @BeforeMethod
   public void setUp() {
