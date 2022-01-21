9ebbc9a96022b2d041aa91249fd8c38a7f4dd1f3
rajoy <rajoy@DESKTOP-18KB1V5.lan>
2022-01-20 20:06:16 -0500

    Handled SSL certifications for EdgeDriver

--- SeleniumTest/src/test/java/com/DriverManager/EdgeDriverManager.java
+++ SeleniumTest/src/test/java/com/DriverManager/EdgeDriverManager.java
@@ -2,12 +2,18 @@ package com.DriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
 public class EdgeDriverManager extends DriverManager {
 
 	public void createDriver() {
 		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\drivers\\msedgedriver.exe");
-		WebDriver driver = new EdgeDriver();
+
+		EdgeOptions options = new EdgeOptions();
+		options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+
+		WebDriver driver = new EdgeDriver(options);
 		this.setDriver(driver);
 	}
 
