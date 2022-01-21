39b578ae53208a14c691c05a6780d1ef68cdab09
rajoy <rajoy@DESKTOP-18KB1V5.lan>
2022-01-20 19:57:42 -0500

    Added ssl handling capability to chromedriver

--- SeleniumTest/src/test/java/com/DriverManager/ChromeDriverManager.java
+++ SeleniumTest/src/test/java/com/DriverManager/ChromeDriverManager.java
@@ -2,12 +2,17 @@ package com.DriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverManager extends DriverManager {
 
 	public void createDriver() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		
+		ChromeOptions options=new ChromeOptions();
+		
+		options.setAcceptInsecureCerts(true);		
+		WebDriver driver = new ChromeDriver(options);
 		this.setDriver(driver);
 
 	}
